f90899162be09dc517f991b7a315cc05dd982370
daniellfeijo <daniellfeijo@gmail.com>
2015-01-17 20:38:45 -0300

    Exercicio 1

---
+++ exercicio1.java
@@ -0,0 +1,40 @@
+class Empresa{
+	String nome;
+	String cnpj;
+	Funcionario[] Funcionarios;
+
+
+}
+
+
+//Classe Funcionário
+//Uma Classe é uma receita de bolo
+//O objeto instanciado é o bolo feito a partir da classe
+class Funcionario{
+	//atributos da classe
+	String nome;
+	String departamento;
+	double salario;
+	Data dataEntrada;
+	String rg;
+	
+	//metodo recebeAumento sem retorno "void"
+	//recebe como parametro o percetual de aumento	
+	void recebeAumento (double percentual){
+		this.salario += this.salario*percentual;
+
+	}
+	//metodo calculaGanhoAnual com retorno double
+	double calculaGanhoAnual(){
+		double ganhoAnual = this.salario*12;
+		return ganhoAnual;		
+	}
+	//metodo para mostrar todos os atributos
+	void mostra(){
+		System.out.println("Nome: "+this.nome);
+		System.out.println("Departamento: "+this.departamento);
+		System.out.println("Salario: "+this.salario);
+		System.out.println("DataEntrada: "+this.dataEntrada.getFormatada());
+		System.out.println("RG: "+this.rg);
+	}
+}
